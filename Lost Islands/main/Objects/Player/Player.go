9981b4e2008c676cdb9c60d595589b28367f72ff
OneWhoBuilds <aidcox2008@gmail.com>
2024-06-22 11:44:07 -0400

    made player more portable
    trying to stop player movement when paused

---
+++ Lost Islands/main/Objects/Player/Player.go
@@ -0,0 +1,117 @@
+components {
+  id: "Player"
+  component: "/main/Scripts/Player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "dust"
+  component: "/Assets/dust.particlefx"
+  position {
+    x: -7.666667
+    y: -16.333332
+    z: 0.0
+  }
+  rotation {
+    x: -3.460301E-4
+    y: 0.010996999
+    z: 0.031448398
+    w: 0.99944484
+  }
+}
+components {
+  id: "Jumping"
+  component: "/Assets/Jumping.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "land"
+  component: "/Assets/land.sound"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "playercollision"
+  component: "/main/Objects/Player/playercollision.collisionobject"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/Objects/Elements/FireBullet.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Assets/Game.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
