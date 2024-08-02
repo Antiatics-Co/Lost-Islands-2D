c6b313f8a6e177b5b1e79c00a8169cd9cbd9e7aa
OneWhoBuilds <aidcox2008@gmail.com>
2024-08-02 14:49:19 -0400

    saving and loading fixed
    Now it just needs to be setup, to act upon the player and the location

--- Lost Islands/main/Objects/IO.go
+++ Lost Islands/main/Objects/IO.go
@@ -1,30 +1,8 @@
 components {
   id: "io"
   component: "/main/GUI files/io.gui"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
 components {
   id: "IO"
-  component: "/main/Scripts/IO.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
+  component: "/main/Scripts/GUI/IO.script"
 }
