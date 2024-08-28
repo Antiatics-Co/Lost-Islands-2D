2c14337e834eee157d17b4cce84b1cc043264298
OneWhoBuilds <aidcox2008@gmail.com>
2024-08-27 22:31:18 -0400

    Collisions
    I am more confused than I was before, no progress has been made

--- Lost Islands/main/Objects/PlayerMap/PlayerMap.go
+++ Lost Islands/main/Objects/PlayerMap/PlayerMap.go
@@ -10,7 +10,7 @@ embedded_components {
   "friction: 0.1\n"
   "restitution: 0.5\n"
   "group: \"playerMap\"\n"
-  "mask: \"map\"\n"
+  "mask: \"/Assets/Images/tile_set.tilesource\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
