88524ffdedc111356a503cd6715757148f0bca30
OneWhoBuilds <aidcox2008@gmail.com>
2024-09-14 17:37:20 -0400

    delete unnecessary files

--- My Project/Objects/IO.go
+++
@@ -1,8 +0,0 @@
-components {
-  id: "io"
-  component: "/main/GUI files/io.gui"
-}
-components {
-  id: "IO"
-  component: "/main/Scripts/IO.script"
-}
--- My Project/main/Objects/Inventory.go
+++
@@ -1,8 +0,0 @@
-components {
-  id: "Inventory"
-  component: "/main/GUI files/Inventory.gui"
-}
-components {
-  id: "inventory"
-  component: "/main/Scripts/Inventory/Inventory.script"
-}
