7ac4183246e7caabe4ede377bfa03c51907e78bd
OneWhoBuilds <aidcox2008@gmail.com>
2024-09-21 23:25:49 -0400

    #81

--- Lost Islands/main/Objects/PlayerMap/OperatorMap.go
+++ Lost Islands/main/Objects/PlayerMap/OperatorMap.go
@@ -3,9 +3,10 @@ embedded_components {
   type: "camera"
   data: "aspect_ratio: 1.0\n"
   "fov: 0.98\n"
-  "near_z: 10.0\n"
-  "far_z: 1000.0\n"
+  "near_z: 0.1\n"
+  "far_z: 50.0\n"
   "auto_aspect_ratio: 1\n"
   "orthographic_projection: 1\n"
+  "orthographic_zoom: 5.0\n"
   ""
 }
