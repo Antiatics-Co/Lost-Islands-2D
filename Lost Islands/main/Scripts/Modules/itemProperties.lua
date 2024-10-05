065706eac0019fead2937f7d04d0c32338803323
OneWhoBuilds <aidcox2008@gmail.com>
2024-10-04 21:18:55 -0400

    #69

--- Lost Islands/main/Scripts/Modules/itemProperties.lua
+++ Lost Islands/main/Scripts/Modules/itemProperties.lua
@@ -2,17 +2,8 @@
 
 local M = {}
 
-local itm = require("main.Scripts.Modules.items")
-
-local BURN = hash("burn")
-local GETEQUIPPED = hash("sendEquipped")
-local DISABLEENERGY = hash("disableEnergy")
-local UNEQUIP = hash("unequip")
-local CALCDAMAGE = hash("sendDamage")
-local SHIELDUP = hash("shield")
 local MAXENERGY = 100
-
-
+local MINENERGY = 0
 local MAXHEALTH = 100
 local MINHEALTH = 1
 
@@ -21,49 +12,23 @@ local equipped = {}
 local shieldUp = false
 local energy = MAXENERGY
 
-function M.isItemEquipped(itemName)
-	if equipped and itemName then
-		return equipped[itemName] ~= nil
-	end
-	return false
-end
-
-
-function M.getHealth()
-	return(health)
-end
-
-
 
 local function useEnergy(usage)
 	energy = energy - usage
 
-	if energy < 0 then
+	if energy < MINENERGY then
 		energy = 0
 	end
 end
 
-function M.recoverEnergy(recoveryAmt)
-	energy = energy + recoveryAmt
+local function getWeaponProperties(items) --ID =  num 
+	--deals with sidearms, mainarms, and elements
 end
 
-function M.getEnergy()
-	return(energy)
-end
-
-function M.equip(item) -- item = {name} 
-	equipped[item.name] = item
-	print("equipped test 1: " .. item.name)
-	print("equipped test 2: " .. equipped[item.name].name)
-end
 
 --the get properties function will be called from setup
 local function getArmourProperties(items) --ID =  num 
 
-end
-
-local function getWeaponProperties(items) --ID =  num 
-	--deals with sidearms, mainarms, and elements
 end
 
 local function getShieldProperties(items) --ID = { name, type }
@@ -102,6 +67,43 @@ local function getDamage(damage)
 	return(damage)
 end
 
+--------------------------------------------------------
+
+function M.isItemEquipped(itemName)
+	if equipped and itemName then
+		return equipped[itemName] ~= nil
+	end
+	return false
+end
+
+function M.getHealth()
+	return(health)
+end
+
+function M.shieldUp()
+	shieldUp = not shieldUp
+end
+
+function M.recoverEnergy(recoveryAmt)
+	energy = energy + recoveryAmt
+end
+
+function M.getEnergy()
+	return(energy)
+end
+
+function M.equip(item) -- item = {name} 
+	equipped[item.name] = item
+	print("equipped test 1: " .. item.name)
+	print("equipped test 2: " .. equipped[item.name].name)
+end
+
+function M.unequip(item)
+	equipped[item.name] = nil
+	print("unequipped: " .. item.name)
+end
+
+
 function M.setHealth(damage)
 	damage = getDamage(damage)
 	health = health - damage
@@ -110,21 +112,6 @@ function M.setHealth(damage)
 	if health < MINHEALTH then
 		print("dead")
 		msg.post("main:/menu#Death", "enable")
-	end
-end
-
-function on_message(self, message_id, message, sender)
-
-	if message_id == UNEQUIP then
-		equipped.remove(message.item)
-	end
-
-	if message_id == CALCDAMAGE then
-		M.setHealth(message.damage)
-	end
-
-	if message_id == SHIELDUP then
-		shieldUp = true
 	end
 end
 
