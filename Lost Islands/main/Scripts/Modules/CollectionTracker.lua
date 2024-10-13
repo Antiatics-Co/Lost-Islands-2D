fa9f8fe2ddfe2d91ae8dbe7ea520dabf0527b20a
OneWhoBuilds <aidcox2008@gmail.com>
2024-10-13 14:15:43 -0400

    fixed #83
    Player can still move in Level for some reason

--- Lost Islands/main/Scripts/Modules/CollectionTracker.lua
+++ Lost Islands/main/Scripts/Modules/CollectionTracker.lua
@@ -2,30 +2,39 @@
 --@description - tracks all loaded collections
 
 -- collection_tracker.lua
-local M = {}
 
+local M = {}
 M.collections = {}
 
 function M.on_message(self, message_id, message, sender)
 	if message_id == hash("proxy_loaded") then
 		M.collections[sender] = true
-		print("Collection loaded:", sender)
+		print("Collection loaded:", tostring(sender))
 	elseif message_id == hash("proxy_unloaded") then
-		M.collections[sender] = false
-		print("Collection unloaded:", sender)
+		M.collections[sender] = nil
+		print("Collection unloaded:", tostring(sender))
 	end
 	M.print_loaded_collections()
 end
 
 function M.is_collection_loaded(collection_url)
-	return M.collections[collection_url] == true
+	print("Checking collection:", tostring(collection_url))
+	M.print_loaded_collections()
+	
+	local temp = false
+	for url, loaded in pairs(M.collections) do
+		if loaded and url == collection_url then
+			temp = true
+		end
+	end
+		return(temp)
 end
 
 function M.print_loaded_collections()
 	print("Loaded collections:")
 	for url, loaded in pairs(M.collections) do
 		if loaded then
-			print(url)
+			print(url, "is loaded.")
 		end
 	end
 end
