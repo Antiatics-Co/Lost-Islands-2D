489b85064dd2e4b44f366631e3131528df354cd8
OneWhoBuilds <aidcox2008@gmail.com>
2024-10-14 01:55:08 -0400

    update

--- Lost Islands/main/Scripts/Modules/CollectionTracker.lua
+++ Lost Islands/main/Scripts/Modules/CollectionTracker.lua
@@ -7,9 +7,13 @@ local M = {}
 M.collections = {}
 
 function M.on_message(self, message_id, message, sender)
+	
+	print("Collection Tracker on message func")
+	
 	if message_id == hash("proxy_loaded") then
 		M.collections[sender] = true
 		print("Collection loaded:", tostring(sender))
+		
 	elseif message_id == hash("proxy_unloaded") then
 	M.collections[sender] = nil
 	print("Collection unloaded:", tostring(sender))
@@ -17,18 +21,17 @@ function M.on_message(self, message_id, message, sender)
 	M.print_loaded_collections()
 end
 
-
 function M.is_collection_loaded(collection_url)
 	print("Checking collection:", tostring(collection_url))
 	M.print_loaded_collections()
 	
-	local temp = false
+	local bool = false
 	for url, loaded in pairs(M.collections) do
 		if loaded and url == collection_url then
-			temp = true
+			bool = true
 		end
 	end
-		return(temp)
+		return(bool)
 end
 
 function M.print_loaded_collections()
