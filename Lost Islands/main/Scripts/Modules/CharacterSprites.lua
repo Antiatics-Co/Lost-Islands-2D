ba1883aeffe8f0a68b2e761b58a4a937cf6c49d7
OneWhoBuilds <aidcox2008@gmail.com>
2024-10-18 17:32:55 -0400

    Create CharacterSprites.lua

---
+++ Lost Islands/main/Scripts/Modules/CharacterSprites.lua
@@ -0,0 +1,67 @@
+-- @title
+-- @Author Aidan Cox
+-- @Description
+--  	This module keeps track of the options the player chooses for
+-- their character sprite
+-- @Version 1
+
+local M = {}
+
+local eyes
+local mouth
+local hair
+local shirt
+local trousers
+local shoes
+
+function M.setEyes(eye)
+	eyes = eyes
+end
+
+function M.setMouth(mou)
+	mouth = mou
+end
+
+function M.setHair(hai)
+	hair = hai
+end
+
+function M.setShirt(shrt)
+	shirt = shrt
+end
+
+function M.setTrousers(pants)
+	trousers = pants
+end
+
+function M.setShoes(shoe)
+	shoes = shoe
+end
+
+-----------------------------------------------------------------
+
+function M.getEyes()
+	return(eyes)
+end
+
+function M.getMouth()
+	return(mouth)
+end
+
+function M.getHair()
+	return(hair)
+end
+
+function M.getShirt()
+	return(shirt)
+end
+
+function M.getTrousers()
+	return(trousers)
+end
+
+function M.getShoes()
+	return(shoes)
+end
+
+return(M)
