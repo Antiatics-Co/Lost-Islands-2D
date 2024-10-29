1631f0fae24013cd78e6f99382bd9dc15e904c57
OneWhoBuilds <aidcox2008@gmail.com>
2024-10-29 19:08:10 -0400

    npc saving, and getting enemy properties

--- Lost Islands/main/Scripts/Modules/NPCProperties.lua
+++ Lost Islands/main/Scripts/Modules/NPCProperties.lua
@@ -11,34 +11,48 @@ local collection = require("main.Scripts.Modules.CollectionTracker")
 
 local M = {}
 
-local npc = {
-		tutorialTown = { --use collecion url as name
-				Mayor = { activity = 0 }, --activity is whatever phase of speaking they have with the player, like if a they complete a quest
-				Mother = { activity = 0 }
+M.npc = {
+	tutorialTown = { --use collecion url as name
+			Mayor = { activity = 0 }, --activity is whatever phase of speaking they have with the player, like if a they complete a quest
+			Mother = { activity = 0 }
 		}
-
-		
-	}
-
-	local enemies = {
-		mainmap = { --use collecion url as name
-			slime = { totalHealth = 35, attack = 5}
-			
-	}
-
-
-	}
-
-function M.calcEnemyHealth(enemy, combo)
-	local damage = 0
-	damage = itm.getMainAttack()
-	damage = damage + itm.getSideAttack(combo)
 	
-	return(enemies[tostring(collection.collections)][enemy].totalHealth - damage)
+}
+	
+local enemies = {
+	MainMap = {
+		slime = { totalHealth = 35, attack = 5}			
+	}
+}
+
+-- Helper function to extract the last part of the URL
+local function get_collection_key(url)
+	local parts = {}
+	for part in string.gmatch(url, "[^/]+") do
+		table.insert(parts, part)
+	end
+	return parts[#parts]
+end
+
+-- Updated calcEnemyHealth and AttackPlayer functions
+function M.calcEnemyHealth(enemy, combo)
+	local damage = itm.getMainAttack() + itm.getSideAttack(combo)
+	local current_collection = get_collection_key(tostring(collection.collections))
+	if enemies[current_collection] and enemies[current_collection][enemy] then
+		return (enemies[current_collection][enemy].totalHealth - damage)
+	else
+		print("Error: Enemy not found in current collection")
+		return nil
+	end
 end
 
 function M.AttackPlayer(enemy)
-	itm.setCombatHealth(enemies[tostring(collection.collections)][enemy].damage)
+	local current_collection = get_collection_key(tostring(collection.collections))
+	if enemies[current_collection] and enemies[current_collection][enemy] then
+		itm.setCombatHealth(enemies[current_collection][enemy].damage)
+	else
+		print("Error: Enemy not found in current collection")
+	end
 end
 
 return(M)
