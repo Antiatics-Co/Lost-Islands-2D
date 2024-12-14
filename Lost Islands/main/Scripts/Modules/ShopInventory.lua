a3a116ec405ba8146978c10d8400d0a7c68c1d2b
OneWhoBuilds <aidcox2008@gmail.com>
2024-12-13 22:03:01 -0500

    grid stuff

--- Lost Islands/main/Scripts/Modules/ShopInventory.lua
+++ Lost Islands/main/Scripts/Modules/ShopInventory.lua
@@ -1,7 +1,18 @@
 local M = {}
 
-local ShopKeeper1 = {  }
+local shops = { 
+	shopKeeper1 = { 
+		amounts = {}
 
 
+	}
+}
 
+function M.getMaxAmounts(keeper)
+	return(shops[keeper].amounts)
+end
+
+function M.setCurrentAmounts(keeper, amounts)
+	shops[keeper].amounts = amounts
+end
 return(M)
